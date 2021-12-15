d8dab75149058af39806242bc346b6e05a696f2f
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2021-12-14 22:27:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 
 import requests
 
-#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-url= "https://w832b3ab81.execute-api.us-east-1.amazonaws.com/Test/predict"
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url= "https://w832b3ab81.execute-api.us-east-1.amazonaws.com/Test/predict"
 
 data = {"url": "https://i.imgur.com/Wj4Lajm.png"}
 
